02de776472967d8651ec39f414d274527bda7103
kieran lavelle <kmplavelle@gmail.com>
2021-03-06 21:00:42 +0000

    Update seed_db.go
    - Added some seeding for resourceTypes
    - Added some seeding for resources
    - Made the script mode robust so it works even when called multiple times. (I.e doesn't try to create a table if it exists and handles CONFLICTS)

--- scripts/seed_db.go
+++ scripts/seed_db.go
@@ -10,7 +10,7 @@ import (
 
 func createGameTable(c *pgx.Conn) {
 	query := `
-		CREATE TABLE game (
+		CREATE TABLE IF NOT EXISTS game (
 			id serial PRIMARY KEY,
 			name text NOT NULL,
 			owner serial NOT NULL,
@@ -26,6 +26,56 @@ func createGameTable(c *pgx.Conn) {
 	}
 }
 
+func createResourceTypes(c *pgx.Conn) {
+	query := `
+		CREATE TABLE IF NOT EXISTS resource_types (
+			id serial PRIMARY KEY,
+			name text UNIQUE NOT NULL
+		);
+	`
+
+	_, err := c.Exec(context.Background(), query)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create table: resource_types: %v\n", err)
+		os.Exit(1)
+	}
+
+	query2 := `INSERT INTO resource_types (name) VALUES ('energy') ON CONFLICT DO NOTHING`
+	_, err = c.Exec(context.Background(), query2)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to populate table: resource_types: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func createResources(c *pgx.Conn) {
+	query := `
+		CREATE TABLE IF NOT EXISTS resources (
+			id serial PRIMARY KEY,
+			type serial NOT NULL REFERENCES resource_types(id),
+			name text UNIQUE NOT NULL,
+			properties json NOT NULL
+		);
+	`
+
+	_, err := c.Exec(context.Background(), query)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create table: resources: %v\n", err)
+		os.Exit(1)
+	}
+
+	query2 := `INSERT INTO resources (type, name, properties) VALUES (
+		1,
+		'Coal Power Station',
+		'{"cost": 1000000, "co2/hr": 100, "workers": 2}'
+	) ON CONFLICT DO NOTHING`
+	_, err = c.Exec(context.Background(), query2)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to populate table: resources: %v\n", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 
 	dbURL := "postgres://postgres:postgres@127.0.0.1:5432/aiville"
@@ -37,4 +87,7 @@ func main() {
 	defer conn.Close(context.Background())
 
 	createGameTable(conn)
+
+	createResourceTypes(conn)
+	createResources(conn)
 }
